046eca5064757796c21cdd6e1f4d745b6c30b6a8
Priya U <149774408+abhisheksingh1000@users.noreply.github.com>
2024-07-25 12:34:15 +0530

    engati chatbot integration

---
+++ clinic-appointment/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.defer = true;
+    script.type = 'text/javascript';
+    script.src = 'https://app.engati.com/static/js/widget.js?config=' + JSON.stringify({
+      bot_key: "",
+      welcome_msg: true,
+      branding_key: "default",
+      server: "https://app.engati.com",
+      e: "p"
+    });
+
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
